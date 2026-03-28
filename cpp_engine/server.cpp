68260fc6f84914151c341dd7f27ce9cf2cbc0333
wskk267 <wskk267@gmail.com>
2026-03-28 17:08:35 +0000

    重构C++ KV引擎，添加WAL日志加载功能，优化PUT和DEL操作的日志记录

--- cpp_engine/server.cpp
+++ cpp_engine/server.cpp
@@ -4,14 +4,34 @@
 #include <arpa/inet.h>
 #include <unordered_map>
 #include <sstream>
+#include <fstream>
 
 using namespace std;
 
 unordered_map<string,string> kv;
+ofstream wal("/item/MiniKV/data/wal.log", ios::app);
+
+void load_wal(){
+    ifstream file("/item/MiniKV/data/wal.log");
+    string line;
+    while(getline(file, line)){
+        stringstream ss(line);
+        string op, key, value;
+        ss >> op >> key;
+        if(op == "PUT"){
+            ss >> value;
+            kv[key] = value;
+        }
+        else if(op == "DEL"){
+            kv.erase(key);
+        }   
+    }
+}
 
 int main() {
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    load_wal();
 
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     sockaddr_in address;
     address.sin_family = AF_INET;
 
@@ -30,7 +50,7 @@ int main() {
 
         char buffer[1024] = {0};
         read(client, buffer, 1024);
-        cout << "Received: " << buffer;
+        cout << "Received: " << buffer << endl;
 
         string cmd(buffer);
         stringstream ss(cmd);
@@ -38,6 +58,8 @@ int main() {
         ss >> op >> key >> value;
         
         if(op == "PUT"){
+            wal << "PUT " << key << " " << value << endl;
+            wal.flush();
             kv[key] = value;
             response = "OK\n";
         }
@@ -49,6 +71,8 @@ int main() {
             }
         }
         else if(op == "DEL"){
+            wal << "DEL " << key << endl;
+            wal.flush();
             if(kv.erase(key)){
                 response = "OK\n";
             } else {
